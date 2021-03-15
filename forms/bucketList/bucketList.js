422f967672350915e253700da8840431aaab72a3
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-15 15:58:32 -0500

    Update bucketList.js

--- forms/bucketList/bucketList.js
+++ forms/bucketList/bucketList.js
@@ -1,18 +1,18 @@
-let userChoice = prompt("enter 1 to see bucket list, enter 2 to add to bucket list")
+let userChoice = Number(prompt("enter 1 to see bucket list, enter 2 to add to bucket list"))
 let bucketList = ["skydiving", "visit europe"]
 let newItem = ''
-let moreData = 'y'
+let moreData = "yes"
 
-while (moreData = 'y'){
+while (moreData ==== "yes"){
 
-if (userChoice = 1) {
-    for (i = 0; i < bucketList.length; i ++) {
-        console.log(bucketList[0])
-        }
-    } else if (userChoice = 2) {
-        newItem = prompt("Enter the item you would like to add")
-        bucketList.push(newItem)
-        }
+    if(userChoice = 1) {
+        for (i = 0; i < bucketList.length; i ++) {
+            console.log(bucketList[0])
+            }
+        } else if(userChoice = 2) {
+            newItem = prompt("Enter the item you would like to add")
+            bucketList.push(newItem)
+            }
         
-        moreData = prompt("enter y to continue")
-        }
+moreData = prompt("enter yes to continue")
+}
